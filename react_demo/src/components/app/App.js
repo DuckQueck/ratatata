cf12026d484ecf8d9db85a8926728810456a9229
Denis <denis7xmp@gmail.com>
2022-11-14 15:14:14 +0200

    Initial commit

---
+++ react_demo/src/components/app/App.js
@@ -0,0 +1,17 @@
+import Header from "../header/Header";
+import Main from "../main/Main";
+import Nav from "../nav/Nav";
+import Footer from "../footer/Footer";
+
+function App() {
+    return (
+        <>
+            <Nav/>
+            <Header/>
+            <Main/>
+            <Footer/>
+            </>
+    );
+}
+
+export default App;
